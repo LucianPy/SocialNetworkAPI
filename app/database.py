90073cee49dd64560003a2d899c2ae9f752fbd02
LucianPy <lucian.pyc@gmail.com>
2021-11-07 01:57:10 +0200

    refactored code and added hashing functionality

--- app/database.py
+++ app/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:dadada12@localhost/SocialNetwork"
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:dadada12@localhost/SocialNetwork'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
--- app/main.py
+++ app/main.py
@@ -1,89 +1,38 @@
 import psycopg2
+import models
+import time
 
+from fastapi import FastAPI
 from psycopg2.extras import RealDictCursor
-from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.param_functions import Body
 
-from pydantic import BaseModel
 
-from typing import Optional
-from random import randrange
+from database import engine
+from routers import post, user
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
 
-try:
-    with open("credentials.txt") as f:
-        lines = f.readlines()
-        user, password = lines[0].strip(), lines[1].strip()
+while True:
 
-    conn = psycopg2.connect(host="localhost", database="SocialNetwork", 
-                            user=user, password=password,
-                            cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("Database connection was succesful")
-except Exception as err:
-    print("Database connection failed")
-    print("Error: ", err)
+    try:
+        conn = psycopg2.connect(host='localhost', database='SocialNetwork', user='postgres',
+                                password='dadada12', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was succesfull!")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print("Error: ", error)
+        time.sleep(2)
+
+
+app.include_router(post.router)
+app.include_router(user.router)
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello Stefan"}
-
-@app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    return {"data": posts}
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
-                  (post.title, post.content, post.published))
-
-    new_post = cursor.fetchone()
-    conn.commit()
-    
-    return {"data": new_post}
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
-    post = cursor.fetchone()
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")
-
-    return {"post_detail": post}
-
-@app.delete("/posts/{id}")
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-
-    if deleted_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")
-    
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
-    
-    updated_post = cursor.fetchone()
-    conn.commit()
-
-    if updated_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"post with id: {id} was not found")
-
-    return {"data": updated_post}
+    return {"message": "Hello World"}
