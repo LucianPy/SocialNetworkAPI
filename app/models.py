5b2ce7816854ba07697d9a15d823bd8d57b91bf6
LucianPy <lucian.pyc@gmail.com>
2021-11-08 17:56:30 +0200

    implemented basic voting functionality

--- app/models.py
+++ app/models.py
@@ -16,7 +16,6 @@ class Post(Base):
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    
     owner = relationship("User")
 
 
@@ -28,3 +27,9 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete = "CASCADE"), primary_key = True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete = "CASCADE"), primary_key = True)
